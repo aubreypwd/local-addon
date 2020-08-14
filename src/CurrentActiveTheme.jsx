8ba1c81189c1ce0c1261f1e0bf9ec170ed1e0cd5
Aubrey Portwood <code@aubreypwd.com>
2020-08-14 15:22:23 -0600

    Reduce everything down.

---
+++ src/CurrentActiveTheme.jsx
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+export default class CurrentActiveTheme extends Component {
+	constructor (props) {
+		super(props);
+
+		this.state = {
+		};
+	}
+
+	componentDidMount () {
+	}
+
+	componentWillUnmount () {
+	}
+
+	render () {
+		return (
+			<div>
+			</div>
+		);
+	}
+
+}
