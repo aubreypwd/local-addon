916c1ea6467c9a115e9d1f102993e825860ef398
Aubrey Portwood <code@aubreypwd.com>
2020-08-15 13:45:46 -0600

    Build our addon and customize branding.

--- src/CurrentActiveTheme.jsx
+++ src/CurrentActiveTheme.jsx
@@ -1,23 +1,56 @@
 import React, { Component } from 'react';
 
+const mysql = require('mysql');
+
 export default class CurrentActiveTheme extends Component {
 	constructor (props) {
 		super(props);
 
 		this.state = {
+			theme: 'Unknown',
 		};
 	}
 
+	getActiveTheme () {
+		if (typeof this.props.site.multisite !== 'undefined') {
+			return;
+		}
+
+		const connection = mysql.createConnection({
+			host: '127.0.0.1',
+			user: this.props.site.mysql.user,
+			password: this.props.site.mysql.password,
+			database: 'local',
+			socketPath: `/${this.props.context.environment.userHome}/Library/Application Support/Local/run/${this.props.site.id}/mysql/mysqld.sock`,
+		});
+
+		connection.connect();
+
+		connection.query("SELECT option_value FROM wp_options WHERE option_name = 'stylesheet'", (error, results, fields) => {
+			this.setState(function (state, props) {
+				return {
+					theme: results[0].option_value ?? this.state.theme,
+				};
+			});
+		});
+
+		connection.end();
+	}
+
 	componentDidMount () {
+		this.getActiveTheme();
 	}
 
 	componentWillUnmount () {
+		// We don't need to do anything when the component is not showing.
 	}
 
 	render () {
 		return (
-			<div>
-			</div>
+			<li class="TableListRow_VfExz_v14-0-1 TableListRow">
+				<strong>Active Theme</strong>
+				<div>{this.state.theme}</div>
+			</li>
 		);
 	}
 
--- src/renderer.jsx
+++ src/renderer.jsx
@@ -1,4 +1,9 @@
-import CurrentActiveTheme from './CurrentActiveTheme.jsx';
+import CurrentActiveTheme from './CurrentActiveTheme';
 
 export default function (context) {
+	const { hooks } = context;
+
+	hooks.addContent('SiteInfoOverview_TableList', (site) => (
+		<CurrentActiveTheme key="local-addon" site={site} context={context} />
+	));
 }
